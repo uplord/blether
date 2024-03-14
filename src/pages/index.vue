c49a5990bec2fdfb791cf1a4ff566300b83d3ffc
Michael <michael@uplord.co.uk>
2024-03-14 20:11:50 +0000

    basic nuxt 3 setup

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <section>
+      <div class="container">
+        <h1>Homepage</h1>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup>
+console.log("Index Page")
+</script>
